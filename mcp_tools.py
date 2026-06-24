4ee7acbba290291aaae63740b6d3e7986a1f1f1e
sandip <sandpipe98@gmail.com>
2026-06-24 14:14:08 +0545

    touch_up

--- mcp_tools.py
+++ mcp_tools.py
@@ -4,34 +4,33 @@ from database_models import Order
 
 mcp = FastMCP("Order MCP System")
 
-
 @mcp.tool()
 def create_order(product: str, quantity: int, price: float):
     db = session()
-
-    order = Order(product=product, quantity=quantity, price=price)
-
-    db.add(order)
-    db.commit()
-    db.refresh(order)
-    db.close()
-
-    return {"order_id": order.id}
-
+    try:
+        order = Order(product=product, quantity=quantity, price=price)
+        db.add(order)
+        db.commit()
+        db.refresh(order)
+        return {"order_id": order.id}
+    finally:
+        db.close()
 
 @mcp.tool()
 def list_orders():
     db = session()
-
-    orders = db.query(Order).all()
-
-    result = [
-        {"id": o.id, "product": o.product, "quantity": o.quantity, "price": o.price}
-        for o in orders
-    ]
-
-    db.close()
-    return result
+    try:
+        orders = db.query(Order).all()
+        return [
+            {"id": o.id, "product": o.product, "quantity": o.quantity, "price": o.price}
+            for o in orders
+        ]
+    finally:
+        db.close()
 
 if __name__ == "__main__":
-    mcp.run(transport="http", host='0.0.0.0', port=8765)
+    mcp.run(
+        transport="http",
+        host="0.0.0.0",
+        port=8765
+    )
